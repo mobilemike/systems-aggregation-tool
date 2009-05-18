458c1b048896e9e3a187da1315c08f19b98d0bf1
Mike Karolow <mobilemike@gmail.com>
2009-05-17 22:48:04 -0400

    Change food_icon helper to use the image_tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,13 @@ module ApplicationHelper
   def food_icon level
     case level
     when 1
-      tag("img", { :src => "images/cabbage_32.png" })
+      image_tag("cabbage_32.png")
     when 2
-      tag("img", { :src => "images/onion_32.png" })
+      image_tag("onion_32.png")
     when 3
-      tag("img", { :src => "images/tomato_32.png" })
+      image_tag("tomato_32.png")
     when 4
-      tag("img", { :src => "images/carrot_32.png" })
+      image_tag("carrot_32.png")
     end
   end
 end
