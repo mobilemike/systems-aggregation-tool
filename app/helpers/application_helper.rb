87faf93d4e937d19daa37b5c4498ef74aaf414a8
Mike Karolow <mobilemike@gmail.com>
2009-05-19 21:33:23 -0400

    Changed icons and index layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,14 @@ module ApplicationHelper
   
   def food_icon level
     case level
+    when 0
+      image_tag("knob_message_32.gif")     
     when 1
-      image_tag("cabbage_32.png")
+      image_tag("cabbage_32.gif")
     when 2
-      image_tag("onion_32.png")
+      image_tag("onion_32.gif")
     when 3
-      image_tag("tomato_32.png")
-    when 4
-      image_tag("carrot_32.png")
+      image_tag("tomato_32.gif")
     end
   end
 end
