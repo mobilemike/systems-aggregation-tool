cafb3a4dce1e9d0c1529350710b27a689d2e10c4
Rails app service account <rails@reitmr.com>
2009-07-14 10:18:53 -0400

    Modified the WSUS reporting parameters

--- app/models/wsus_computer.rb
+++ app/models/wsus_computer.rb
@@ -3,9 +3,9 @@ class WsusComputer < ActiveRecord::Base
   
   def update_health
     case updates_outstanding
-      when 0..1 then 1
-      when 2..3 then 2
-      when 4..(1.0/0) then 3
+      when 0 then 1
+      when 1..2 then 2
+      when 3..(1.0/0) then 3
     end
   end
 
