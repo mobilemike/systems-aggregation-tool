de880907eb7d72008d8d8b01f27afb9e2367e20c
Mike Karolow <mobilemike@gmail.com>
2009-09-14 15:44:12 -0400

    Workaround for staging routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :owners
 
   map.resources :computers, :active_scaffold => true, :member => { :health => :get }
+  map.resources :computers, :as => 'cmdb/computers', :member => { :health => :get }
   
   map.root :controller => "computers"
 end
--- public/javascripts/active_scaffold/default/active_scaffold.js
+++ public/javascripts/active_scaffold/default/active_scaffold.js
@@ -372,7 +372,6 @@ ActiveScaffold.ActionLink.Record.prototype = Object.extend(new ActiveScaffold.Ac
         if (this.target.hasClassName('even-record')) new_target.addClassName('even-record');
         this.target = new_target;
         this.close();
-        generateHealthTooltips();
       }.bind(this),
 
       onFailure: function(request) {
