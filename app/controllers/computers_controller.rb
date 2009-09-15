d33c00bc7a43be07d047b170cd7ab1dac8385352
Mike Karolow <mobilemike@gmail.com>
2009-09-15 17:58:14 -0400

    Added column head tooltips

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -7,14 +7,21 @@ class ComputersController < ApplicationController
     c.columns[:health].includes = [:wsus_computer, :akorri_server_storage, :scom_computer,
                                    :epo_computer, :vmware_computer]
     c.columns[:health].label = "<img src=\"#{ActionController::Base.relative_url_root}/images/cabbage_16.gif\" />"
+    c.columns[:health].description = "Overall system health"
     c.columns[:wsus_computer].sort_by :method => 'wsus_computer ? wsus_computer.updates_outstanding : 0.1'
     c.columns[:wsus_computer].label = "<img src=\"#{ActionController::Base.relative_url_root}/images/band_aid.png\" />"
+    c.columns[:wsus_computer].description = "Outstanding WSUS patches"    
     c.columns[:name].sort_by :method => 'name'
+    c.columns[:name].description = "DNS name"
+    c.columns[:owner].description = "Assigned owner's intials"   
     c.columns[:domain].sort_by :method => 'domain || String.new'
+    c.columns[:domain].description = "DNS domain"   
     c.columns[:virtual?].label = "<img src=\"#{ActionController::Base.relative_url_root}/images/vmware.gif\" />"
     c.columns[:virtual?].sort_by :method => 'virtual? ? 1 : 0'
+    c.columns[:virtual?].description = "Virtual or Physical"
     c.columns[:ip].label = "IP"
     c.columns[:ip].sort_by :sql
+    c.columns[:ip].description = "Primary IP"   
     c.list.sorting = [{:health => :desc}]
     c.list.per_page = 20
   end
