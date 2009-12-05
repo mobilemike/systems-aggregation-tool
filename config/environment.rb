7b1b7ae0c89ac8442ab349ec8d3d68332d907d9a
Mike Karolow <mobilemike@gmail.com>
2009-12-04 22:26:48 -0500

    Removed activerecord-sqlserver adaptor from the environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'dbi', :version => '0.4.1'
   config.gem 'dbd-odbc', :version => '0.2.4', :lib => 'dbd/ODBC'
-  config.gem 'activerecord-sqlserver-adapter'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
