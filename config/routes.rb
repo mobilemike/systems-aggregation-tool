a5ee9362710c79230982daca914b5d43e47f2e2b
Mike Karolow <mobilemike@gmail.com>
2010-06-13 18:30:40 -0400

    Reconfigured config files, routes, sync files and application.js for the new salad.mis.local conventions as well as the new -stage and -dev sites

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,6 @@ ActionController::Routing::Routes.draw do |map|
   map.owner 'computers/owner/:owner_initials', :controller => 'computers', :action => 'index'
   map.owner_status 'computers/owner/:owner_initials/status/:status', :controller => 'computers', :action => 'index'
   map.status 'computers/status/:status', :controller => 'computers', :action => 'index'
-  map.resources :computers,
-                :active_scaffold => true,
-                :as => 'cmdb/computers',
-                :member => { :health => :get, :chart => :get },
-                :has_many => :issues
   
   map.root :controller => "computers"
 end
