19661cf4e4e980cc323e0c1cc5f5743f37b5db26
Mike Karolow <mobilemike@gmail.com>
2010-11-21 21:48:40 -0500

    Added show action link back to servers for now

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -73,7 +73,7 @@ class ServersController < ApplicationController
     c.columns[:us_group_name].label                  = 'WSUS Group'
 
     
-    c.actions.exclude :create, :delete, :nested, :show
+    c.actions.exclude :create, :delete, :nested
     
     c.update.link        = false
 
